181cb795296ccfcae4e87004e6826b5e579e1d60
Luiz F Dias <luizfd@gmail.com>
2023-09-29 21:34:01 -0300

    Produtos style update

--- src/routes/Home/produto/livros/index.tsx
+++ src/routes/Home/produto/livros/index.tsx
@@ -2,10 +2,10 @@ import './styles.css'
 
 export default function Livros(){
     return(
-        <>
+        <div className='prod-content-container'>
         <h3>Livro 1</h3>
         <h3>Livro 2</h3>
         <h3>Livro 3</h3>
-        </>
+        </div>
     );
 }
